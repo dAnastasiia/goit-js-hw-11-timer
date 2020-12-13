45e6a92da57c10770a40b30607ecb3d4f6cf0a32
DrAnastasiia <dr.anastasiyya0@gmail.com>
2020-12-13 16:48:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -5,12 +5,46 @@ const refs = {
   secs: document.querySelector('[data-value="secs"]'),
 };
 
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+class CountdownTimer {
+  constructor({ targetDate, updateTime }) {
+    this.updateTime = updateTime;
+    this.targetDate = targetDate;
+  }
+
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTime(deltaTime);
+      this.updateTime(time);
+    }, 1000);
+  }
+
+  getTime(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new CountdownTimer({
+  targetDate: new Date('Dec 25, 2020'),
+  updateTime: updateTime,
 });
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+function updateTime({ days, hours, mins, secs }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
+
+timer.start();
